232df2e6de4508628f8465d19bbbb202bbafb8ea
Ana Helena <anahelenarp@hotmail.com>
2021-08-03 11:25:27 -0300

    Adding files

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d0544e9bb8bb2ac5826e18b8907fcb5'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f131ee2f-c7be-46ee-a79b-3a7d33c8b4f7'
+};
