bf928950d69c46543b905247d026eb651875bf53
Ana Helena <anahelenarp@hotmail.com>
2021-08-09 19:52:38 -0300

    Congi heroku

--- app/config/server.js
+++ app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('HEROKU_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0d0544e9bb8bb2ac5826e18b8907fcb5'),
