ccaf753784a91cc74116c03b57dcd815e8ce8d28
Ana Helena <anahelenarp@hotmail.com>
2021-08-11 08:47:23 -0300

    Aws s3 plugin

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2f122e158b1256357d8338e329915dbe'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '65a581e4-ece0-428c-83ef-ab120323c5ba'
+};
